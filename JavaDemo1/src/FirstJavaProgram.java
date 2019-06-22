2a20fa52e0a935b26126d5c62ca52b53908f2381
abdul666 <abdulnayeemna6@gmail.com>
2019-06-22 22:45:05 +0530

    Example3

---
+++ JavaDemo1/src/FirstJavaProgram.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author abdul.nethibottu
+ *
+ */
+public class FirstJavaProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("First java program");
+	}
+
+}
