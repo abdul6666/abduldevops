7e9f5f23e6adbd400980879e77744d89fc1142da
abdul666 <abdulnayeemna6@gmail.com>
2019-06-22 23:02:21 +0530

    First java program

--- JavaDemo1/src/FirstJavaProgram.java
+++ JavaDemo1/src/FirstJavaProgram.java
@@ -14,6 +14,8 @@ public class FirstJavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("First java program");
+     
+     System.out.println("dfjsdfkskjfsklfsjklfklsfk");
 	}
 
 }
